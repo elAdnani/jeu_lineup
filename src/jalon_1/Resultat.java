794ec74bc3f5edff9d366251e6f787b8006f56ae
adnan <adnan@PCSP19>
2021-04-15 16:25:59 +0200

    Création de la classe Résultat

---
+++ src/jalon_1/Resultat.java
@@ -0,0 +1,33 @@
+/*
+ * This file is licensed to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package jalon_1;
+
+/**
+ * Cette classe sert à .........
+ *
+ * @author 
+ * IUT-A Informatique, Universite de Lille.
+ * @date 14 avr. 2021
+ * @version XX
+ */
+public class Resultat {
+
+	/**
+	 * 
+	 */
+	public Resultat() {
+		// TODO Auto-generated constructor stub
+	}
+}

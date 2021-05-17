903d9f56b9d72e76863f2f345c489fa3f931f25c
Selmène Choukhi <choukhiselmene@gmail.com>
2021-05-17 23:08:33 +0200

    Mise à jour de la classe Parametres.

--- src/lineup_3/Parametres.java
+++ src/lineup_3/Parametres.java
@@ -1,7 +1,8 @@
 package lineup_3;
 
 /**
- * Cette classe fait.........
+ * Cette classe permet de rassembler tous les paramètres d'une partie pour une meilleure efficacité lors d'initialisations de Plateau,
+ * de calculs de déplacement, d'ajouts de piège etc... Les autres objets s'appuient sur les attributs de Parametres pour fonctionner.
  *
  * @author <a href="mailto:choukhiselmene@gmail.com">Selmene CHOUKHI</a>
  * IUT-A Informatique, Universite de Lille.
@@ -9,5 +10,48 @@ package lineup_3;
  * @version 28 avr. 2021 13:35:52
  */
 public class Parametres {
-	// TODO mettre tout les paramètres d'avant partie.
+	// TODO mettre tous les paramètres d'avant partie.
+
+		// Class Attributes
+	/**
+	 * Correspond au nombre de couche choisi pour la partie.
+	 */
+	private final int NBCOUCHE;
+	
+	/**
+	 * Correspond au nombre de côté choisi pour la partie.
+	 */
+	private final int NBCOTE;
+	
+	/**
+	 * Correspond au choix de mettre des pièges ou non.
+	 */
+	private final boolean PIEGES;
+	
+		// Constructor
+	
+	public Parametres(int nbCouche, int nbCote, boolean piege) {
+		this.NBCOUCHE = nbCouche;
+		this.NBCOTE = nbCote;
+		this.PIEGES = piege;
+	}
+
+		// Methods
+	
+	public int getNBCOUCHE() {
+		return NBCOUCHE;
+	}
+
+	public int getNBCOTE() {
+		return NBCOTE;
+	}
+
+	public boolean isPIEGES() {
+		return PIEGES;
+	}
+	
+	public int getNBPOINT() {
+		return (2*this.NBCOTE)*this.NBCOUCHE;
+	}
+	
 }

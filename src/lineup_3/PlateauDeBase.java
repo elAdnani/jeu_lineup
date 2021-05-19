83005541f58a58d609533445b6b7c0ff938d97c4
Adnan Kouakoua <adnan.kouakoua.etu@univ-lille.fr>
2021-05-19 13:18:49 +0200

    Modification de la classe Plateau respectant les consignes

--- src/lineup_3/PlateauDeBase.java
+++ src/lineup_3/PlateauDeBase.java
@@ -26,9 +26,10 @@ import java.util.Set;
 
 
 /**
- * Cette classe représente un plateau respectant les consignes :- Trois Sommets par côtés
- * 																- Trois couches
- * 																- Autant de côté possible
+ * Cette classe représente un plateau respectant les consignes : </br>
+ * - Trois Sommets par côtés </br>
+ * - Trois couches	</br>
+ * - Autant de côté possible </br></br>
  *
  * @author <a href="mailto:adnan.kouakoua.etu@univ-lille.fr">Adnân KOUAKOUA</a>
  *         IUT-A Informatique, Université de Lille.
@@ -36,59 +37,53 @@ import java.util.Set;
  * @version v1-2
  */
 
-public class PlateauDeBase extends Plateau{
+public class PlateauPolynomial extends Plateau{
 
 	/**
-	 * Attributs composé du nombre de côté (forme du plateau)
-	 * d'un nombre de couche pré-déterminé pour ce type de Plateau
-	 * Liste des pions des joueurs et un graphe qui correspond au squelette du plateau
+	 * Attributs composé : du nombre de côté (forme du plateau)</br>
+	 * d'un nombre de couche pré-déterminé pour ce type de Plateau </br>
+	 * Liste des pions des joueurs et un graphe qui correspond au squelette du plateau</br>
 	 */
 	
 	private  int        nbcote  ;
 	public  static final int   NBCOUCHE = 3;
-	private  Set<Pion>  listPion;
 	
-	public final Graphe grph;
+	public final Graphe<Case> grph;
 	
 	
 	/**
-	 * Construction d'un Plateau de nombre de côté défini
+	 * Construction d'un Plateau de nombre de côté défini </br>
+	 * On le définit en tant que graphe non orienté.
 	 * @param nombreCote
 	 */
 	
-	public PlateauDeBase(int nombreCote) {
+	public PlateauPolynomial(int nombreCote) {
 		
-		this(nombreCote, new Graphe("Plateau", GrapheType.UGRAPH), new HashSet<>());
+		this( nombreCote , new Graphe<Case> ("Plateau", GrapheType.UGRAPH) );
+	
 	}
-	
+
 	/**
-	 * Construction d'un Plateau de nombre de côté défini et à partir d'un graph définie
-	 * @param nombreCote
-	 * @param grph
-	 */
-	
-	public PlateauDeBase(int nombreCote , Graphe grph) {
-		
-		this(nombreCote, grph, new HashSet<>());
-	}
-	
-	/**
-	 * Construction d'un Plateau de nombre de côté défini et à partir d'un graph définie et d'une liste de joueur
+	 * Construction d'un Plateau de nombre de côté défini et à partir d'un graphe déjà construit. </br>
+	 * Il sera toujours possible de construire des sommets par la suite. Mais des sommets peuvent déjà être présent.
 	 * @param nombreCote
 	 * @param grph
 	 * @param joueursDuJeu
 	 */
 	
-	public PlateauDeBase(int nombreCote, Graphe grph, Set<Joueur> joueursDuJeu) {
-		
-		super(joueursDuJeu);  
-		this.listPion = new HashSet<>();
-		this.grph = grph;
+	public PlateauPolynomial(int nombreCote, Graphe<Case> grph) {
+		if(grph.getType()==GrapheType.UGRAPH) {
+			this.grph = grph;
+		}
+		else {
+			this.grph= new Graphe<Case> ("Plateau", GrapheType.UGRAPH);
+		}
 		this.nbcote = nombreCote;
 	}
 	
 	/**
-	 * 
+	 * Génère le nombre de pions qu'il existe dans un polynome. </br>
+	 *
 	 * @return nombre de pion qu'il est possible d'avoir dans une couche
 	 */
 	public int getNbPionMax() {
@@ -97,42 +92,6 @@ public class PlateauDeBase extends Plateau{
 	
 
 	
-	/**
-	 * Permet à partir de coordonnée, de retrouver le pion appartenant au plateau
-	 * @param x
-	 * @param y
-	 * @return le pion qui correspond un pion ou null, en fonction de la correspondance des coordonnées dans la liste des sommets
-	 */
-	public Pion retrouverPion (int x, int y) {
-
-		for( Pion p :listPion)
-			
-			if( p.getCoordonnees().getX()==x && p.getCoordonnees().getY()==y)
-				
-				return p;
-		
-		return null;
-	}
-	
-	
-	
-	/**
-	 * L'ensemble des composantes du plateau
-	 * 
-	 * @return donne la présentation des joueurs ainsi que leurs pions.
-	 */
-	
-	@Override
-	public String toString() {
-		
-		String result="";
-			for(Joueur j : listJoueur)
-				
-				result= result + "["+ j.toString()+ "]\n";
-
-			
-		return result ;
-	}
 	
 	/**
 	 * Est destiné a être l'affichage du plateau permettant de jouer
@@ -150,17 +109,13 @@ public class PlateauDeBase extends Plateau{
 			}
 			
 			
-			
-			
-			
 		}
 		return plateauEnString;
 	}
 
 
 	/**
-	 * génère un plateau de jeu en utilisant les méthodes du graphes Donnes tous les
-	 * sommets d'un plateau (toutes les coordonnée existantes)
+	 * Génère tous les sommets du plateau, à partir du nombre de pion maximum qu'il peut y avoir. </br>
 	 * 
 	 */
 
@@ -171,30 +126,30 @@ public class PlateauDeBase extends Plateau{
 
 			for (int j = 0; j < this.getNbPionMax(); j++)
 
-				this.grph.addNode(String.format("%02d", i) + String.format("%02d", j) );
-				
+				this.grph.addNode(new Case(i,j) );
 		
+		System.out.println(this.getListSommet());
 		// génération des arêtes
 		generationListArret(this.grph.getEdges());
 	}
 	
 	/**
-	 * génère les arêtes à partir des sommets
-	 * @param listSommet
+	 * Génère les arêtes à partir des sommets. </br>
+	 * Regarde pour chaque sommet, la proximité avec d'autres sommets. </br>
+	 * Si elle remarque qu'un sommet est voisin d'un autre, alors son arêtes est créées.
+	 * @param listeDesSommets
 	 */
-	private void generationListArret(Map<String, List<String>> listeDesSommets) {
+	private void generationListArret(Map<Case, List<Case>> listeDesSommets) {
+		
+		for ( Case sommet : grph.getNodes() ) {
 
-		for ( String sommet : grph.getNodes() ) {
-			
-			for ( String voisin : grph.getNodes() ) 
-				
+			for ( Case voisin : grph.getNodes() ) {
+
 				if(estVoisin(sommet, voisin)) 
-					
-					grph.addEdge(sommet, voisin);
-
-		}
-				
 			
+					grph.addEdge(sommet, voisin);
+			}
+		}
 	}
 	
 	/**
@@ -204,29 +159,30 @@ public class PlateauDeBase extends Plateau{
 	 * @return
 	 */
 	
-	private boolean estVoisin(String coordonnee_un, String coordonnee_deux) {
-		
-		Paire coor1= new Paire(Integer.parseInt( coordonnee_un.substring(0,2) ),   Integer.parseInt(  coordonnee_un.substring( 2,coordonnee_un.length() )  )   );
-		Paire coor2= new Paire(Integer.parseInt( coordonnee_deux.substring(0,2) ),   Integer.parseInt(  coordonnee_deux.substring( 2,coordonnee_deux.length() )  )   );
-		
-		return estVoisin(coor1, coor2);
+	private boolean estVoisin(Case coordonnee_un, Case coordonnee_deux) {
+		System.out.println(" coo 1 : " +coordonnee_un.toString());
+		return estVoisin(coordonnee_un.coordonnee, coordonnee_deux.coordonnee);
 	}
 	
 	/**
-	 * Appelle la méthode estVoisin qui return et vérifie la proximité des deux sommets
+	 * Vérifie la proximité des deux paires de coordonnée. </br>
+	 * Et dit selon les consignes du plateau, si elles sont voisines. </br>
+	 * C'est à dire : </br>
+	 *  - Si elles sont l'un à côté de l'autre dans  un même polygone </br>
+	 *  - Si elles sont au milieu du plateau à des couches voisine
 	 * @param point_un
 	 * @param point_deux
 	 * @return
 	 */
 	
 	public boolean estVoisin (Paire point_un, Paire point_deux) {
-		
+
 		return estVoisin(point_un.getX(),point_un.getY(),     point_deux.getX(), point_deux.getY());
 	}
 	
 
 	/**
-	 * Vérifie la proximité des deux points de part les coordonnée
+	 * Vérifie la proximité des deux points à partir de deux types de coordonnées mathématiques
 	 * @param x1 premier sommet
 	 * @param y1 premier sommet
 	 * @param x2 deuxième sommet
@@ -234,6 +190,7 @@ public class PlateauDeBase extends Plateau{
 	 * @return true s'il est possible de passer d'un sommet à un autre directement, false ou sinon
 	 */
 	private boolean estVoisin(int x1, int y1, int x2, int y2) {
+		
 		
 		if(x1==x2 && ( (y1==(y2-1 + this.getNbPionMax())%this.getNbPionMax() ) || (y1==(y2+1)%this.getNbPionMax() ))){
 			// si elles se retrouvent sur la même couche et qu'elles ont une différence de  coordonnée +1 ou -1 en y
@@ -247,184 +204,31 @@ public class PlateauDeBase extends Plateau{
 		return false;
 	}
 	
-	
+	public int retrouverSommet(Paire coordonnee) {
+		for( Case sommet : this.getListSommet()) {
+			if(sommet.coordonnee.equals(coordonnee))
+				return this.getListSommet().indexOf(sommet);
+		}
+		return -1;
+	}
 	/**
-	 * permet à partir de coordonnée de retrouver un sommet (va dans la liste des sommets et donne l'indice du sommet qui correspond au coordonnee)
+	 * permet à partir de coordonnée de retrouver un sommet. </br>
+	 * On cherche à partir de la liste des sommets, les coordonnées correspondant à celui indiqué en paramètre.
 	 * @param x
 	 * @param y
 	 * @return
 	 */
-	public int retrouverSommet (int x, int y) {
-
-		return this.getListSommet().indexOf( String.format("%02d", x) + String.format("%02d", y) );	
+	
+	protected int retrouverSommet (int x, int y) {
+		return retrouverSommet( new Paire(x, y) );
+	
 	}
-
-	
-	
-	
-	/**
-	 * Elle permet de voir si la partie est finie
-	 * @return s'il existe au moins un alignement de trois pions alors la partie est gagnée
-	 */
-	
-	public Joueur partieGagnee() {
-		
-		if( lierInterieur()==null) { 
-			
-			return lierExterieur();
-		}
-		
-		return lierInterieur();
-		
-	}
-
-	/**
-	 * Regarde pour chaque pion, ayant la même coordonnée X, si celui qui est juste
-	 * avant et juste après existe
-	 * 
-	 * @return joueur si l'élèment juste avant et après existe dans la liste des
-	 *         sommets
-	 * @return joueur si la boucle for each a parcouru toute la liste des sommets
-	 *         sans trouver d'alignement.
-	 *        @TODO changer modulo en un autre nom, j'ai la flemme de le faire ;(	
-	 */
-	
-	private Joueur lierInterieur() {
-		
-		int modulo = ( this.getNBCOTE() - 1) * NBCOUCHE;
-		for(Joueur j : super.listJoueur)
-			
-				for (int i = 0; i < NBCOUCHE; i++){
-					
-					for (Pion p1 : j.getMain().getMain())
-						
-						for (Pion p2 : j.getMain().getMain())
-							
-							for (Pion p3 : j.getMain().getMain())
-								
-								if (p1.getX() == i)
-									
-									if (p1.getX() == ((p2.getX() - 1) % modulo) && p1.getX() == (p3.getX() - 2) % modulo)
-										
-										return j;
-		
-				}
-		return null;
-	}
-
-	/**
-	 * Regarde pour chaque pion, ayant la même coordonnée Y, si celui qui est juste
-	 * avant et juste après existe
-	 * 
-	 * @return true si l'élèment juste avant et après existe dans la liste des
-	 *         sommets
-	 * @return false si la boucle for each a parcouru toute la liste des sommets
-	 *         sans trouver d'alignement.
-	 */
-	
-	private Joueur lierExterieur() {
-		
-		for (int i = 0; i < NBCOUCHE; i++){
-			
-			for(Joueur j : super.listJoueur)
-				for (Pion p1 : j.getMain().getMain())
-				
-					for (Pion p2 : j.getMain().getMain())
-					
-						for (Pion p3 : j.getMain().getMain())
-						
-							if (p1.getY() == i)
-							
-								if (p1.getY() == ((p2.getY() - 1) % NBCOUCHE) && p1.getY() == (p3.getY() - 2) % NBCOUCHE)
-								
-									return j;
-									
-			
-		}
-		
-		return null;
-	}
-
-
-	/**
-	 * Permet d'ajouter un élément pion à notre liste de sommet
-	 * 
-	 * @param pion
-	 */
-	
-	public void addPion(Pion pion) {
-		
-		listPion.add(pion);
-		
-		
-	}
-
-	/**
-	 * Permet d'enlever un élément pion à notre liste de sommet
-	 * 
-	 * @param pion
-	 */
-	
-	public void removePion(Pion pion) {
-		
-		listPion.remove(pion);
-	}
-
-	/**
-	 * Ajouter un joueur dans la liste de joueur des plateaux, puis on rajoute leurs pions dans la liste des pions
-	 * @param utilisateur
-	 */
-	public void addJoueur(Joueur utilisateur) {
-		
-		listJoueur.add(utilisateur);
-		if(utilisateur.getMain()!=null)
-			for(Pion nouveauPionDuJoueur : utilisateur.getMain().getMain()) 
-				this.listPion.add(nouveauPionDuJoueur);
-			
-	}
-	
-	/**
-	 * Ajouter un joueur dans la liste de joueur des plateaux, puis on rajoute leurs pions dans la liste des pions
-	 * @param utilisateur
-	 */
-	public void removeJoueur(Joueur utilisateur) {
-		
-		listJoueur.remove(utilisateur);
-		if(utilisateur.getMain()!=null)
-			for(Pion pionDuJoueur : utilisateur.getMain().getMain()) 
-				this.listPion.remove(pionDuJoueur);
-			
-	}
-	
-	
 	
 
 	// --------------------------------- GETTER ET SETTERS // ---------------------------------\\
 	
 	/**
-	 * 
-	 * @return la liste des sommets
-	 */
-	
-	public Set<Pion> getListPion() {
-		
-		return  this.listPion;
-	}
-
-	/**
-	 * 
-	 * @param listSommet la liste de sommet à définir
-	 */
-	
-	public  void setListSommet(Set <Pion> listPion) {
-		
-		this.listPion = listPion;
-	}
-
-
-
-	/**
-	 * 
+	 * Return le nombre de côté que ce plateau possède
 	 * @return le NBCOTE
 	 */
 	
@@ -433,13 +237,20 @@ public class PlateauDeBase extends Plateau{
 		return this.nbcote;
 	}
 
-	
-	public List<String> getListSommet() {
+	/**
+	 * la liste de sommet que ce plateau possède
+	 * @return 
+	 */
+	public List<Case> getListSommet() {
 		
 		return this.grph.getNodes();
 	}
 	
-	public Map<String, List<String>> getListArret() {
+	/**
+	 * la liste des arêtes que ce plateau possède
+	 * @return
+	 */
+	protected Map< Case, List<Case> > getListArret() {
 		
 		return this.grph.getEdges();
 	}

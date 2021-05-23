5742238263b2ba533390d4888f80c996b9d61489
Alexis Bonal <alexis.bonal.etu@univ-lille.fr>
2021-05-23 22:58:41 +0200

    JavaDoc de la classe Paire

--- src/lineup_3/Paire.java
+++ src/lineup_3/Paire.java
@@ -1,24 +1,28 @@
 package lineup_3;
 
 /**
- * Cette classe regroupe deux entiers x et y sous forme de paire.<br>
- * L'intérêt de faire ceci est de simplifier l'utilisation de coordonnées en deux dimensions, tels que pour l'emplacement d'un point dans un canvas ou ses coordonnées de plateau ou la taille d'une matrice par exemple.<br>
- * De plus, de nombreuses méthodes sont ajoutées et permettent de simplifier certaines tâches.
- * Telles que la somme des attributs, la moyenne de ceux-ci, la possibilité de les inverser, d'ajouter ou enlever une valeur à l'un d'eux. Des opérations entre deux paires sont également réalisables.
+ * <p>Une paire de deux entiers</p>
+ * <p>Cette paire regroupe deux entiers x et y.</p>
+ * <p>Cela permet de simplifier l'utilisation de coordonnées en deux dimensions, tels que pour l'emplacement d'un point dans un canvas, ses coordonnées de plateau ou la taille d'une matrice par exemple.</p>
+ * <p>
+ * 	De plus, de nombreuses méthodes sont ajoutées et permettent de simplifier certaines tâches.<br>
+ * 	Telles que la somme des attributs, la moyenne de ceux-ci, la possibilité de les inverser, d'ajouter ou enlever une valeur à l'un d'eux.<br>
+ * 	Des opérations entre deux paires sont également réalisables.
+ * </p>
  * 
  * @author <a href="mailto:alexis.bonal.etu@univ-lille.fr">BONAL Alexis</ a>
  */
 public class Paire {
 	/* ATTRIBUTS ______________________________ */
 		/**
-		 * Le premier attribut de la paire.
-		 * Peut correspondre aux abscisses dans un repère ou à la couche d'un plateau à laquelle se trouve un pion
+		 * <p>Le premier attribut de la paire.</p>
+		 * <p>Peut correspondre aux abscisses dans un repère ou à la couche d'un plateau à laquelle se trouve un pion</p>
 		 */
 		private int x;
 		
 		/** 
-		 * Le second attribut de la paire.
-		 * Peut correspondre aux ordonnées dans un repère ou à l'emplacement sur une couche où le pion se trouve.
+		 * <p>Le second attribut de la paire.</p>
+		 * <p>Peut correspondre aux ordonnées dans un repère ou à l'emplacement sur une couche où le pion se trouve.</p>
 		 */
 		private int y;
 		
@@ -28,10 +32,10 @@ public class Paire {
 		
 	/* CONSTRUCTEUR ______________________________ */
 		/**
-		 * Initialisation d'une paire d'entiers en fonction des entiers passés en paramètre.
+		 * Constructeur d'une paire avec deux entiers.
 		 * 
-		 * @param first le premier entier de la paire, soit x.
-		 * @param second le second entier de la paire, soit y.
+		 * @param first  Le premier entier de la paire, soit x.
+		 * @param second Le second entier de la paire, soit y.
 		 */
 		public Paire(int first, int second) {
 			x = first;
@@ -44,37 +48,32 @@ public class Paire {
 		
 	/* ACCESSEURS ET MUTATEURS ______________________________ */
 		/**
-		 * Permet d'obtenir le premier attribut
+		 * Récupère le premier attribut.
 		 * 
-		 * @return la valeur du premier entier
+		 * @return La valeur du premier entier
 		 */
-		public int getX() {
-			return x;
-		}
+		public int getX() { return x; }
 	
 		/**
-		 * Permet de définir la valeur du premier attribut
-		 * @param first
+		 * Modifie le premier attribut.
+		 * 
+		 * @param first Le premier attribut, soit x.
 		 */
-		public void setX(int first) {
-			x = first;
-		}
+		public void setX(int first) { x = first; }
 	
 		/**
-		 * Permet d'obtenir le deuxième attribut
-		 * @return la valeur du deuxième entier
+		 * Récupère deuxième attribut.
+		 * 
+		 * @return La valeur du deuxième entier
 		 */
-		public int getY() {
-			return y;
-		}
+		public int getY() { return y; }
 	
 		/**
-		 * Permet de définir la valeur du deuxième attribut
-		 * @param second
+		 * Modifie le deuxième attribut.
+		 * 
+		 * @param second Le second attribut, soit y.
 		 */
-		public void setY(int second) {
-			y = second;
-		}
+		public void setY(int second) { y = second; }
 		
 		
 		
@@ -82,9 +81,9 @@ public class Paire {
 		
 	/* HASHCODE ET EQUALS ______________________________ */
 		/**
-		 * Renvoie une valeur de hash code pour la paire courante.
+		 * Génère un hash de la paire.
 		 * 
-		 * @return un hash code pour la paire courante.
+		 * @return Un hash code.
 		 */
 		@Override
 		public int hashCode() {
@@ -100,11 +99,13 @@ public class Paire {
 	
 	
 		/**
-		 * Compare l'objet spécifié en paramètre avec la paire courante.<br>
-		 * Renvoie true si un objet est comparé avec lui-même, et si tous leurs attributs correspondent lorsque le paramètre est bien une paire.
+		 * <p>Compare deux paires.</p>
+		 * <p>Renvoie true si un objet est comparé avec lui-même, et si tous leurs attributs correspondent lorsque le paramètre est bien une paire.</p>
 		 * 
-		 * @param obj - l'objet de référence avec lequel comparer la paire courante.
-		 * @return true si la paire est la même que l'objet passé en paramètre.
+		 * @param obj L'objet à comparer avec la paire.
+		 * 
+		 * @return - true si la paire est la même que l'objet passé en paramètre<br>
+		 *         - false s'ils sont différents
 		 */
 		@Override
 		public boolean equals(Object obj) {
@@ -122,6 +123,7 @@ public class Paire {
 			
 			// finalement, on vérifie que tous les éléments correspondent bien entre eux.
 			Paire other = (Paire) obj;
+			
 			return x == other.x && y == other.y;
 		}
 		
@@ -131,15 +133,14 @@ public class Paire {
 		
 	/* TOSTRING ______________________________ */
 		/**
-		 * Renvoie une chaîne de caractères contenant la représentation de la paire courante.<br>
-		 * Sous la forme suivante : (x; y)
+		 * <p>Génère une représentation de la paire</p>
+		 * <p>Renvoie une chaîne de caractères contenant la représentation de la paire courante.</p>
+		 * <p>Sous la forme suivante : (x; y)</p>
 		 * 
-		 * @return une représentation sous forme de chaîne de caractères d'une paire.
+		 * @return Une chaîne de caractères contenant la représentation d'une paire.
 		 */
 		@Override
-		public String toString() {
-			return "(" + x + "; " + y + ")";
-		}
+		public String toString() { return "(" + x + "; " + y + ")"; }
 		
 		
 		
@@ -147,7 +148,9 @@ public class Paire {
 		
 	/* MÉTHODES ______________________________ */
 		/**
-		 * Permet d'inverser les deux valeurs de la paire courante.
+		 * Inverse les deux valeurs d'une paire.
+		 * 
+		 * @return Une paire aux attributs échangés.
 		 */
 		public Paire inverser() {
 			int save = x;
@@ -158,27 +161,26 @@ public class Paire {
 		}
 		
 		/**
-		 * Effectue la somme des attributs de la paire courante et renvoie le résultat 
+		 * Somme des attributs
 		 * 
 		 * @return la somme des attributs
 		 */
-		public int somme() {
-			return x + y;
-		}
+		public int somme() { return x + y; }
 	
 		/**
-		 * Effectue la moyenne des attributs de la paire courante et renvoie le résultat
+		 * Moyenne des attributs
 		 * 
 		 * @return la moyenne
 		 */
-		public double moyenne() {
-			return somme() / 2.0;
-		}
+		public double moyenne() { return somme() / 2.0; }
 		
 		/**
-		 * Effectue la somme de la paire courante avec une paire donnée en paramètre et modifie la paire courante.
+		 * <p>Somme de deux paires</p>
+		 * <p>La paire courante est modifiée et le résultat est également renvoyé.</p>
 		 * 
-		 * @param terme2 - paire à ajouter à la paire courante
+		 * @param terme2 La paire à ajouter.
+		 * 
+		 * @return Une paire résultant de la somme.
 		 */
 		public Paire sommeAvec(Paire terme2) {
 			ajouterX(terme2.x);
@@ -188,9 +190,12 @@ public class Paire {
 		}
 		
 		/**
-		 * Effectue la soustraction de la paire courante avec une paire donnée en paramètre et modifie la paire courante.
+		 * <p>Soustraction de deux paires</p>
+		 * <p>La paire courante est modifiée et le résultat est également renvoyé.</p>
 		 * 
-		 * @param terme2 - paire à enlever à la paire courante
+		 * @param terme2 La paire à enlever.
+		 * 
+		 * @return Une paire résultant de la soustraction.
 		 */
 		public Paire soustractionAvec(Paire terme2) {
 			enleverX(terme2.x);
@@ -200,9 +205,11 @@ public class Paire {
 		}
 		
 		/**
-		 * Permet d'augmenter la valeur du premier argument avec la valeur du paramètre donné.
+		 * <p>Augmente le premier argument</p>
 		 * 
-		 * @param i - la valeur à ajouter au premier argument.
+		 * @param i La valeur à ajouter.
+		 * 
+		 * @return Une paire.
 		 */
 		public Paire ajouterX(int i) {
 			x += i;
@@ -211,9 +218,11 @@ public class Paire {
 		}
 		
 		/**
-		 * Permet de réduire la valeur du premier argument avec la valeur du paramètre donné.
+		 * <p>Réduis le premier argument</p>
 		 * 
-		 * @param i - la valeur à enlever au premier argument.
+		 * @param i - la valeur à enlever.
+		 * 
+		 * @return Une paire.
 		 */
 		public Paire enleverX(int i) {
 			ajouterX(-i);
@@ -222,9 +231,11 @@ public class Paire {
 		}
 		
 		/**
-		 * Permet d'augmenter la valeur du deuxième argument avec la valeur du paramètre donné.
+		 * <p>Augment le second argument</p>
 		 * 
-		 * @param i - la valeur à ajouter au deuxième argument.
+		 * @param i La valeur à ajouter.
+		 * 
+		 * @return Une paire.
 		 */
 		public Paire ajouterY(int i) {
 			y += i;
@@ -233,9 +244,11 @@ public class Paire {
 		}
 		
 		/**
-		 * Permet de réduire la valeur du deuxième argument avec la valeur du paramètre donné.
+		 * <p>Réduis le second argument</p>
 		 * 
-		 * @param i - la valeur à enlever au deuxième argument.
+		 * @param i La valeur à enlever.
+		 * 
+		 * @return Une paire.
 		 */
 		public Paire enleverY(int i) {
 			ajouterY(-i);
@@ -244,9 +257,11 @@ public class Paire {
 		}
 		
 		/**
-		 * Permet d'augmenter la valeur de chacun des arguments avec la valeur du paramètre donné.
+		 * <p>Augmente les arguments</p>
 		 * 
-		 * @param i - la valeur à ajouter aux arguments.
+		 * @param i La valeur à ajouter.
+		 * 
+		 * @return Une paire.
 		 */
 		public Paire ajouter(int i) {
 			ajouterX(i);
@@ -256,9 +271,11 @@ public class Paire {
 		}
 		
 		/**
-		 * Permet de réduire la valeur de chacun des arguments avec la valeur du paramètre donné.
+		 * <p>Réduis les arguments</p>
 		 * 
-		 * @param i - la valeur à enlever aux arguments.
+		 * @param i La valeur à enlever.
+		 * 
+		 * @return Une paire.
 		 */
 		public Paire enlever(int i) {
 			ajouter(-i);

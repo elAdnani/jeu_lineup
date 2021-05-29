521d0a6720d4a433af3d7324b7071f5b4869bdad
Selmène Choukhi <choukhiselmene@gmail.com>
2021-05-29 03:32:20 +0200

    Petit commit nécessaire.

--- src/lineup_3/GrapheMatrice.java
+++ src/lineup_3/GrapheMatrice.java
@@ -335,10 +335,8 @@ public class GrapheMatrice<T> extends Graphe<T> {
 			int indiceS = ensembleSommets.indexOf(sommet);
 			
 			for (int i = 0; i < matrice.getTaille().getX(); i++) 
-				if (matrice.read(indiceS, i) != 0 && indiceS != i) {
+				if (matrice.read(indiceS, i) != 0 && indiceS != i)
 					voisins.add(ensembleSommets.get(i));
-					System.out.println("dedans");
-				}
 			
 			if (type.isDirected())
 				for (int i = 0; i < matrice.getTaille().getX(); i++) 

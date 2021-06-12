34f2acd3b2336c892362371d529e7167f93f57de
Adnan Kouakoua <adnan.kouakoua.etu@univ-lille.fr>
2021-06-13 01:02:14 +0200

    up

--- bin/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"lineup_3.controleur.gestionDesEvenements"},{"l":"lineup_3.modele"},{"l":"lineup_3.visuel"},{"l":"outils"},{"l":"outils.jeu"}];updateSearchResults();
